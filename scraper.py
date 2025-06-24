b033391dee274bb79cf72fca07d1400c5fbae8d8
Shashwat <shashwatakhileshshukla@gmail.com>
2025-06-24 22:41:40 +0530

    cookie bug fix

--- scraper.py
+++ scraper.py
@@ -1,24 +1,37 @@
 from apify_client import ApifyClient
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_cookies_from_env():
+    """Get cookies from environment variables"""
+    cookies = []
+    cookie_mappings = {
+        "li_at": os.getenv("LI_AT_COOKIE"),
+        "JSESSIONID": os.getenv("JSESSIONID_COOKIE"),
+        "bcookie": os.getenv("BCOOKIE_COOKIE"),
+        "bscookie": os.getenv("BSCOOKIE_COOKIE"),
+        "lidc": os.getenv("LIDC_COOKIE")
+    }
+    for name, value in cookie_mappings.items():
+        if value:
+            cookies.append({"name": name, "value": value})
+    
+    return cookies
 
 def scrape_linkedin_profile(url: str):
     client = ApifyClient(os.environ.get("TOKEN_APIFY"))
 
     run_input = {
-        "urls": [url],  
-        "cookie": [
-            {"name": "li_at", "value": "AQEDAT_oPWECobv9AAABltVftpMAAAGXu_g5PU0Aq35x4j4qntisplf4p68Qn_1vHzONf_8Gr48iwuPz21_3V5BBVJlaIaMFdlPj5H7bkhWs3xD3l4wVReOpmnWRCA6jHZSVbqkrCDhr7aj6IyOd_x6-"},
-            {"name": "JSESSIONID", "value": "ajax:2226503378999304052"},
-            {"name": "bcookie", "value": "v=2&d8191c3b-646e-4b39-8755-7a2618b66678"},
-            {"name": "bscookie", "value": "v=1&20250303163900965906ca-de2f-48c9-8e83-1264586cabdaAQGIlmSD1E1h26oc7mE3Q8v4yi-RZMr5"},
-            {"name": "lidc", "value": "b=VB73:s=V:r=V:a=V:p=V:g=3851:u=340:x=1:i=1750602933:t=1750679761:v=2:sig=AQGhc5Rf_Z9TmigU1ovCC-9iEl2y8I8w"}
-        ],
-        "minDelay": 15,
-        "maxDelay": 60,
-        "proxy": {
-            "useApifyProxy": True,
-        },
-        "findContacts": False
+    "urls": [url],
+    "cookie": get_cookies_from_env(),
+    "minDelay": 15,
+    "maxDelay": 60,
+    "proxy": {
+        "useApifyProxy": True,
+    },
+    "findContacts": False
     }
 
     try:
